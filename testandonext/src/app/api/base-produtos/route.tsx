47bf30d25a892f532f4749cab9881f40f1ad849a
leonardobarbosas <leonardobarbosantos@gmail.com>
2024-09-25 22:36:22 -0300

    criando uma api

---
+++ testandonext/src/app/api/base-produtos/route.tsx
@@ -0,0 +1,10 @@
+import { NextResponse } from "next/server";
+import { promises as fs } from "fs";
+
+export async function GET() {
+  const file = await fs.readFile(process.cwd() + "/src/data/base.json", "utf-8");
+
+  const dados = JSON.parse(file);
+
+  return NextResponse.json(dados);
+}
