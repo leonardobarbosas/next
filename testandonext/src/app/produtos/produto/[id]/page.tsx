7935f0c2315940a17c41bcb411e00256c924ab13
leonardobarbosas <leonardobarbosasantos@gmail.com>
2024-09-26 22:46:17 -0300

    mexendo na tag imagem

--- testandonext/src/app/produtos/produto/[id]/page.tsx
+++ testandonext/src/app/produtos/produto/[id]/page.tsx
@@ -1,9 +1,18 @@
 "use client";
 import { TipoProduto } from "@/types";
+import Image from "next/image";
 import { useEffect, useState } from "react";
 
 export default function Produto({ params }: { params: { id: number } }) {
-  const [produto, setProduto] = useState<TipoProduto>();
+  const [produto, setProduto] = useState<TipoProduto>({
+    id: 0,
+    nome: "",
+    preco: 0,
+    marca: "",
+    modelo: "",
+    descricao: "",
+    imagem: "",
+  });
 
   useEffect(() => {
     const chamadaDaApi = async () => {
@@ -22,6 +31,13 @@ export default function Produto({ params }: { params: { id: number } }) {
       <h1>Produto</h1>
       <p>ID: {produto?.id}</p>
       <p>Nome: {produto?.nome}</p>
+      <p>Preço: {produto?.preco}</p>
+      <p>Marca: {produto?.marca}</p>
+      <p>Modelo: {produto?.modelo}</p>
+      <p>Descrição: {produto?.descricao}</p>
+      <figure>
+        <Image src={produto?.imagem} alt={produto?.nome} width={100} height={100}/>
+      </figure>
     </div>
   );
 }
